0b06a51626b66dce9f179d8b3bd4de428206ce1c
rajiv256 <rajivpensidpri@gmail.com>
2021-11-07 13:21:12 -0500

    Preprocessing the dataset done.

--- configs.py
+++ configs.py
@@ -2,3 +2,17 @@ import os
 
 ROOT_DIR = os.getcwd()
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
+LABEL_MAP = {
+    'SUPPORTS': 1,
+    'REFUTES': 0
+}
+SPECIAL_CHARS = {
+    "NEWLINE": "\n",
+    "SPACE": " ",
+    "TAB": "\t"
+}
+SPECIAL_NUMS = {
+    "NEG_ONE": -1,
+    "ZERO": 0,
+    "ONE": 1
+}
