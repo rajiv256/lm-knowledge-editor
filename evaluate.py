0f737a23c07740a975a2f4750ca4272be286d1bc
rajiv256 <rajivpensidpri@gmail.com>
2021-11-07 10:01:57 -0500

    Second commit. Adding files.

--- evaluate.py
+++ evaluate.py
@@ -7,6 +7,8 @@ tokenizer = AutoTokenizer.from_pretrained("nielsr/coref-bert-base")
 model = AutoModel.from_pretrained("nielsr/coref-bert-base")
 
 # Load FEVER dataset
-fever_dataset = load_dataset("fever", "wiki_pages")
+fever_dataset = load_dataset("fever", "v1.0")
+paper_dev = fever_dataset['paper_dev']
+paper_test = fever_dataset['paper_test']
 
-print(fever_dataset['wikipedia_pages'][1])
+print(type(paper_dev))
